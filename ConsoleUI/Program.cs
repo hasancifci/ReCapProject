75f7c5a3867372d9b5650eb4cfb64f88b3f698a2
hasancifciiHasan <hasancifcii@outlook.com>
2021-02-14 23:46:35 +0300

    10.ödev

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,32 +12,74 @@ namespace ConsoleUI
         {
             //Test1();
 
-            CarManager carManager = new CarManager(new EfCarDal());
+            //Test2();
 
-            foreach (var car in carManager.GetCarDetail())
+            //Ödev10Test();
+
+        }
+
+        private static void Ödev10Test()
+        {
+            UserManager userManager = new UserManager(new EfUserDal());
+
+            userManager.Add(new User
             {
-                Console.WriteLine(car.Description + " - " + car.BrandName + " - " + car.ColorName + " - " + car.DailyPrice );
-            }
+                Id = 1,
+                FirstName = "Hasan",
+                LastName = "Çifçi",
+                Email = "hhasancifci@gmail.com",
+                Password = "1234"
+            });
+
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+
+            customerManager.Add(new Customer
+            {
+                Id = 1,
+                UserId = 1,
+                CompanyName = "HECE YAZILIM"
+            });
+
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+
+            rentalManager.Add(new Rental
+            {
+                Id = 1,
+                CarId = 1,
+                CustomerId = 1,
+                RentDate = new DateTime(2020, 02, 22),
+                ReturnDate = new DateTime(2020, 02, 28),
+            });
+        }
+
+        private static void Test2()
+        {
+            //CarManager carManager = new CarManager(new EfCarDal());
+
+            //foreach (var car in carManager.GetCarDetail())
+            //{
+            //    Console.WriteLine(car.Description + " - " + car.BrandName + " - " + car.ColorName + " - " + car.DailyPrice);
+            //}
         }
 
         private static void Test1()
         {
-            CarManager carManager = new CarManager(new EfCarDal());
-            ColorManager colorManager = new ColorManager(new EfColorDal());
-            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            //CarManager carManager = new CarManager(new EfCarDal());
+            //ColorManager colorManager = new ColorManager(new EfColorDal());
+            //BrandManager brandManager = new BrandManager(new EfBrandDal());
 
 
-            colorManager.Add(new Color { Id = 4, ColorName = "Yeşil" });
+            //colorManager.Add(new Color { Id = 4, ColorName = "Yeşil" });
 
-            brandManager.Add(new Brand { Id = 5, BrandName = "Opel" });
+            //brandManager.Add(new Brand { Id = 5, BrandName = "Opel" });
 
-            carManager.Add(new Car { Id = 7, ColorId = 4, BrandId = 5, ModelYear = 2015, DailyPrice = 350, Description = "Opel Kiralama Bedeli" });
+            //carManager.Add(new Car { Id = 7, ColorId = 4, BrandId = 5, ModelYear = 2015, DailyPrice = 350, Description = "Opel Kiralama Bedeli" });
 
 
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine(car.Description);
-            }
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);              
+            //}
         }
     }
 }
